d735f0473ad1a649dc95654e6b58b24a814f3258
Nikhila-Zen <nikhilavalli@gozennith.com>
2025-10-05 21:01:54 +0000

    issues fixed

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
 import { updateSession } from './lib/supabase/middleware'
 import { createClient } from './lib/supabase/server'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const { data: { user } } = await createClient().auth.getUser()
 
   if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return Response.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
   if (user && request.nextUrl.pathname.startsWith('/login')) {
-    return Response.redirect(new URL('/dashboard', request.url))
+    return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
   return await updateSession(request)
